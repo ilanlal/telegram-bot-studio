84f7bf5adda92099ddc4ac5c05813d0df9fbfa8c
Ilan Laloum <isramail@gmail.com>
2026-01-23 03:58:24 +0200

    refactor: remove WidgetModel and its tests to simplify the codebase

--- src/components/models/index.js
+++ src/components/models/index.js
@@ -1,12 +1,10 @@
 const { SheetModel } = require('./SheetModel');
 const { LoggerModel } = require('./LoggerModel');
-const { WidgetModel } = require('./WidgetModel');
 const { EnvironmentModel } = require('./EnvironmentModel');
 const { CustomerModel } = require('./CustomerModel');
 
 // Export all models
 global.SheetModel = SheetModel;
 global.LoggerModel = LoggerModel;
-global.WidgetModel = WidgetModel;
 global.EnvironmentModel = EnvironmentModel;
 global.CustomerModel = CustomerModel;
